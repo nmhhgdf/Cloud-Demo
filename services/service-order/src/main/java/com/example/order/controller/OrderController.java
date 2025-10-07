efbc93caa6129c37b8df73187841bd9f24e353f1
Spiral <120575257@qq.com>
2025-10-07 11:39:40 +0800

    Sentinel熔断规则、热点规则

--- services/service-order/src/main/java/com/example/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/example/order/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.example.order.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.example.order.bean.Order;
 import com.example.order.properties.OrderProperties;
 import com.example.order.service.OrderService;
@@ -34,11 +36,21 @@ public class OrderController {
     }
 
     @GetMapping("/seckill")
-    public Order seckill(@RequestParam("userId") Long userId,
-                             @RequestParam("productId") Long productId) {
+    @SentinelResource(value = "seckill-order", fallback = "seckillFallback")
+    public Order seckill(@RequestParam(value = "userId", required = false) Long userId,
+                             @RequestParam(value = "productId", defaultValue = "1000") Long productId) {
         Order order = orderService.createOrder(productId, userId);
         order.setId(Long.MAX_VALUE);
         return order;
+    }
+
+    public Order seckillFallback(Long userId, Long productId, Throwable ex) {
+        log.error("seckillFallback...");
+        return Order.builder()
+                .id(productId)
+                .userId(userId)
+                .address("异常信息:" + ex.getClass())
+                .build();
     }
 
     @GetMapping("/writeDb")
