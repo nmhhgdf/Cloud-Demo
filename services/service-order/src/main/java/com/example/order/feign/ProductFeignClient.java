81a2f7da76205ad4219fd33db2837514785de873
Spiral <120575257@qq.com>
2025-10-17 21:43:40 +0800

    Gateway请求路由

--- services/service-order/src/main/java/com/example/order/feign/ProductFeignClient.java
+++ services/service-order/src/main/java/com/example/order/feign/ProductFeignClient.java
@@ -3,14 +3,12 @@ package com.example.order.feign;
 import com.example.order.feign.fallback.ProductFeignClientFallback;
 import com.example.product.bean.Product;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(value = "service-product", fallback = ProductFeignClientFallback.class)
 public interface ProductFeignClient {
 
-    @GetMapping("/product/{id}")
+    @GetMapping("/api/product/product/{id}")
     Product getProductById(@PathVariable("id") Long id,
                            @RequestHeader("token") String token);
 
--- services/service-product/src/main/java/com/example/product/controller/ProductController.java
+++ services/service-product/src/main/java/com/example/product/controller/ProductController.java
@@ -7,8 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/product")
 @Slf4j
 @RestController
 public class ProductController {
