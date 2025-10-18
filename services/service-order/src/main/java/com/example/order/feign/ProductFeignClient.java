269d781b12907cdc2156aa6b8114b7a59042a1c2
Spiral <120575257@qq.com>
2025-10-18 19:50:31 +0800

    Gateway完结-过网关写法（不建议）

--- services/service-order/src/main/java/com/example/order/feign/ProductFeignClient.java
+++ services/service-order/src/main/java/com/example/order/feign/ProductFeignClient.java
@@ -6,8 +6,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(value = "service-product", fallback = ProductFeignClientFallback.class)
+//@FeignClient(value = "gateway", fallback = ProductFeignClientFallback.class)  #这两个注释是为服务间调用过网关的写法，不建议使用
 public interface ProductFeignClient {
 
+//    @GetMapping("/api/product/product/{id}")  #这两个注释是为服务间调用过网关的写法，不建议使用
     @GetMapping("/product/{id}")
     Product getProductById(@PathVariable("id") Long id,
                            @RequestHeader("token") String token);
